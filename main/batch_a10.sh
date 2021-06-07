e8af60a77e82cb8f249a8bf7d077d23a99bbbc6e
Lei Fu <lei.fu2020@gmail.com>
2021-06-06 20:03:56 -0700

    update batch submission file

---
+++ main/batch_a10.sh
@@ -0,0 +1,10 @@
+source /data/intel_fpga/devcloudLoginToolSetup.sh
+tools_setup -t A10DS
+
+# Running project in FPGA Hardware Mode (this takes approximately 1 hour)
+printf "\\n%s\\n" "Running in FPGA Hardware Mode:"
+cd FPGA-acceleration-of-epidemic-simulation/main
+aoc device/main_general.cl -o bin/main_fpga.aocx -board=pac_a10 -report
+
+
+
